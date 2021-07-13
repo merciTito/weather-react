1233b40bc9f0be4ca9f0c8da403d90c2eb96f227
merciTito <60163383+merciTito@users.noreply.github.com>
2021-07-13 03:09:04 -0400

    updated app.js with Github link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        SheCodes Homework
+        <a href="https://github.com/merciTito/weather-react" target="_blank">
+          and is Open-sourced on Git-hub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
