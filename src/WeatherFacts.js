8e30c63930121fbffd88fe744a0c9a93dc204624
merciTito <60163383+merciTito@users.noreply.github.com>
2021-07-22 05:56:16 -0400

    created new component, and added the celsius & fahrenheit

--- src/WeatherFacts.js
+++ src/WeatherFacts.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherFacts(props) {
   return (
@@ -14,16 +16,12 @@ export default function WeatherFacts(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearFix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
             <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°c</span>
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
---
+++ src/WeatherIcon.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherIcon(props) {
+  const codeMapping = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "PARTLY_BROKEN CLOUDS",
+    "04n": "PARTLY_BROKEN CLOUDS",
+    "09d": "SHOWER_RAIN",
+    "09n": "SHOWER_RAIN",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "THUNDERSTORM",
+    "11n": "THUNDERSTORM",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "MIST",
+    "50n": "MIST",
+  };
+
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="#0d6efd"
+      size={props.size}
+      animate={true}
+    />
+  );
+}
